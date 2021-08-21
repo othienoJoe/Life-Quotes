fb10d77ac3841467dbfbf018f02c67f4ec70f502
othienoJoe <bryo094@gmail.com>
2021-08-21 20:09:47 +0300

    modified app component section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Life-Quotes';
+  quotes:Quote[] = [
+    {id:1, name:'Abraham Lincoln'},
+    {id:2, name:'Abraham Lincoln'},
+    {id:3, name:'Abraham Lincoln'},
+    {id:4, name:'Abraham Lincoln'},
+    {id:5, name:'Abraham Lincoln'},
+    {id:6, name:'Abraham Lincoln'},
+    {id:7, name:'Abraham Lincoln'},
+  ];
 }
