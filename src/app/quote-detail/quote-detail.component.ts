2b638187f4c125531d603d67b31f082c86b4ffd4
othienoJoe <bryo094@gmail.com>
2021-08-22 14:56:07 +0300

    removed bugs from quote-detail component and modified it

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,8 +1,9 @@
-import { Quote } from '@angular/compiler';
+// import { Quote } from '@angular/compiler';
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
-  selector: 'app-quote-detail',
+  selector: 'app-quote-detail-component',
   templateUrl: './quote-detail.component.html',
   styleUrls: ['./quote-detail.component.css']
 })
@@ -10,10 +11,13 @@ export class QuoteDetailComponent implements OnInit {
 
   @Input() quote: Quote | undefined;
   @Output() isComplete = new EventEmitter<boolean>();
-  
+
+  quoteComplete(complete:boolean) {
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,19 +9,24 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1, 'Abraham Lincoln', ''),
-    new Quote(2, 'Abraham Lincoln', ''),
-    new Quote(3, 'Abraham Lincoln', ''),
-    new Quote(4, 'Abraham Lincoln', ''),
-    new Quote(5, 'Abraham Lincoln', ''),
-    new Quote(6, 'Abraham Lincoln', ''),
-    new Quote(7, 'Abraham Lincoln', ''),
+    new Quote(1, 'Dolly Parton', 'The way I see it if you want the Rainbow you gotta put up with the rain'),
+    new Quote(2, 'Mae West', ''),
+    new Quote(3, 'Thomas A. Edison', ''),
+    new Quote(4, 'Seneca', ''),
+    new Quote(5, 'Will Smith', ''),
+    new Quote(6, 'Steve Jobs', ''),
+    new Quote(7, 'Morris West', ''),
   ];
 
   toggleDetails(index:any) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
 
+  completeQuote(isComplete:any, index:any) {
+    if (isComplete) {
+      this.quotes.splice(index,1);
+    }
+  }
   constructor() { }
 
   ngOnInit() {
