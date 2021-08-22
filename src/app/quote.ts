0cd149fcbc005c1dbe1be66d3395ba5c84dfca8c
othienoJoe <bryo094@gmail.com>
2021-08-22 17:05:15 +0300

    modified the date-count pipe to align with the root component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,7 @@
+
 export class Quote {
-  public showDescription: boolean;
-  constructor(public id: number,public name: string,public description: string,public completeDate: Date) {
+  showDescription: boolean;
+  constructor(public id: number,public name: string,public title: string,public quote: string,public author:string, public datePosted: Date,public likes:number, public dislikes:number) {
     this.showDescription=false;
   }
 }
